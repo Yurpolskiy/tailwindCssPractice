e7865a2a3d2a649b23c6bc25de638a93f21ef0cd
Yurpolskiy <fpbidni@gmail.com>
2024-03-01 19:12:40 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+    } else {
+    document.documentElement.classList.remove('dark')
+}
+
+
+
+(function imageCreator() {
+  let laptop = document.querySelector('.imgWrapper')
+  let imageElement = document.createElement("img")
+  document.documentElement.classList.contains('dark') ? imageElement.src = './images/screensaverBlack.png' : imageElement.src = '/src/images/screensaverWhite.png'
+  laptop.appendChild(imageElement)
+}())
